de5afcef928f2e34a5da3cc09f1bbc39ffcb9dd4
Student <you@example.com>
2023-07-31 08:57:06 +0300

    React-Movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Components/getmovies/NavBar';
+import GetMovies from './Components/getmovies';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MovieDetails from './Components/getmovies/Details/MovieDetls';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <BrowserRouter>
+        <Navbar />
+       
+        <Routes>
+          <Route path='/' element={<GetMovies />} />
+          <Route path='/movies/:movieId' element={<MovieDetails />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
